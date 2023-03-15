2f6544e7367e614e3fabd238305e51e457cf07db
carlos-a-g-h <carloselputohabanero@proton.me>
2023-03-15 14:26:36 -0400

    Update bbq.rs

--- bbq.rs
+++ bbq.rs
@@ -1,37 +1,37 @@
 struct Queue
 {
-    name: String,
-    data: Vec<String>,
+		name: String,
+		data: Vec<String>,
 }
 
 impl Queue
 {
-    fn new(name: String) -> Queue
-    {
-        Queue
-        {
-            name: name,
-            data: Vec::new(),
-        }
-    }
+	fn new(name: String) -> Queue
+	{
+		Queue
+		{
+			name: name,
+			data: Vec::new(),
+		}
+	}
 
-    fn add_item(&mut self,value: String)
-    {
-        self.data.push(value);
-    }
+	fn push(&mut self,value: String)
+	{
+		self.data.push(value);
+	}
 
-    fn show(&self)
-    {
-        println!("Contents of {}: {:?}",self.name,self.data);
-    }
+	fn print(&self)
+	{
+		println!("Queue: {}\n\tContents: {:?}\n",self.name,self.data);
+	}
 }
 
 fn main()
 {
-    let mut q=Queue::new(String::from("Test"));
-    q.show();
-    q.add_item(String::from("thing 1"));
-    q.show();
-    q.add_item(String::from("thing 2"));
-    q.show();
+	let mut q=Queue::new(String::from("Test"));
+	q.print();
+	q.push(String::from("uwu"));
+	q.print();
+	q.push(String::from("lol"));
+	q.print();
 }
