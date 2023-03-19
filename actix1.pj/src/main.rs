6c784e0e0982beb85535f648ab9a1bc9c0e81022
carlos-a-g-h <carloselputohabanero@proton.me>
2023-03-19 14:21:51 -0400

    Update main.rs

--- actix1.pj/src/main.rs
+++ actix1.pj/src/main.rs
@@ -1,21 +1,40 @@
 use actix_web::{get, web, App, HttpServer, Responder};
 
 #[get("/")]
-async fn index() -> impl Responder
+async fn root() -> impl Responder
 {
-	"Hello, World!"
+	"Welcome to our conglomerate\n\n/store/\n/store/name\n/store/name/index"
 }
 
-#[get("/{name}")]
-async fn hello(name: web::Path<String>) -> impl Responder
+#[get("/stores")]
+async fn root_stores(name: web::Path<String>) -> Result<String>
 {
-	format!("Hello {}!", &name)
+	Ok("Requested all stores")
+}
+
+#[get("/stores/{name}")]
+async fn from_store_get_all(name: web::Path<String>) -> Result<String>
+{
+	Ok(format!("Requested all items from the store \"{}\"",&name))
+}
+
+#[get("/stores/{name}/{index}")]
+async fn from_store_get_index(values: web::Path<(String,u32)>) -> Result<String>
+{
+	let (name,index)=values.info_inner();
+	Ok(format!("Requested item at position {} from the store \"{}\"",index,name))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()>
 {
-	HttpServer::new(|| App::new().service(index).service(hello))
+	println!("Listening at 8080...")
+	HttpServer::new(|| App::new()
+		.service(root)
+		.service(root_stores)
+		.service(from_store_get_all)
+		.service(from_store_get_index)
+		)
 		.bind(("127.0.0.1", 8080))?
 		.run()
 		.await
