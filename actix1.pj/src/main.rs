1efb9315ab3ba5287a95350b01051b8ec62654b3
carlos-a-g-h <carloselputohabanero@proton.me>
2023-03-21 10:28:09 -0400

    Update main.rs

--- actix1.pj/src/main.rs
+++ actix1.pj/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use actix_web::{get, post, web, App, HttpServer, Responder, HttpResponse};
 use actix_web::http::StatusCode;
 use serde::{Serialize, Deserialize};
-use serde_json::json;
+use serde_json;
 
 // Queue struct
 
