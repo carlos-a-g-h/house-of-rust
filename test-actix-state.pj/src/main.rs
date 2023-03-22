7062676ae85739606927bfdc6b041c1dd682b9d1
carlos-a-g-h <carloselputohabanero@proton.me>
2023-03-22 13:32:13 -0400

    Update main.rs

--- test-actix-state.pj/src/main.rs
+++ test-actix-state.pj/src/main.rs
@@ -10,23 +10,24 @@ struct AppStateWithCounter
 
 async fn index(data: web::Data<AppStateWithCounter>) -> String
 {
-		//get counter's MutexGuard
-		let mut counter=data.counter.lock().unwrap();
-		// access counter inside MutexGuard
-		//response with count
-		println!("counter = {:?}",counter);
-		"look at the terminal"
+	//get counter's MutexGuard
+	let mut counter=data.counter.lock().unwrap();
+	// access counter inside MutexGuard
+	//response with count
+	println!("counter = {:?}",counter);
+	"Look at the terminal"
 }
 
-async fn add(data: web::Data<AppStateWithCounter>) -> String
+async fn add_one(data: web::Data<AppStateWithCounter>) -> String
 {
 	let mut counter=data.counter.lock().unwrap();
-	*counter.push("yes");
+	*counter.push("another_one".to_string());
 	"Added one"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+	println!("Actix Web (Rust) demo at 8080\n\nTopic: Application Mutable State")
 	// Note: web::Data created _outside_ HttpServer::new closure
 	let counter = web::Data::new(AppStateWithCounter {
 		counter: Mutex::new(
@@ -39,6 +40,7 @@ async fn main() -> std::io::Result<()> {
 		App::new()
 			.app_data(counter.clone()) // <- register the created data
 			.route("/", web::get().to(index))
+			.route("/add", web::get().to(add_one))
 	})
 	.bind(("127.0.0.1", 8080))?
 	.run()
